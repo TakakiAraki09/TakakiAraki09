2d17a218fa00afa2157500a9cd77972863a635d3
ArakiTakaki09 <arakitakaki.work+main@gmail.com>
2024-11-19 16:53:32 +0900

    良い感じ

--- packages/web-application/app/page.tsx
+++ packages/web-application/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { Github } from 'github-repository';
 import { Antict } from "antict-repository";
+import { Theme } from 'theme';
 
 export default function Page() {
   return (
     <div>
       <Antict />
       <Github />
+      <Theme />
     </div>
   );
 }

7eaf3d540fd709369b9f75edf3b4bb77edbaef9f
ArakiTakaki09 <arakitakaki.work+main@gmail.com>
2024-11-21 04:46:40 +0900

    update

--- packages/theme/src/injections/GoogleTagManager.tsx
+++ packages/theme/src/injections/GoogleTagManager.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 import Script from "next/script";
 
 const TAG_ID = 'GTM-MS7Q6JFP'
-export const GoogleTagManager = (props: {
-  tagId?: string;
-}) => {
-  return (
+
+export const GoogleTagManager = (tagId: string = TAG_ID) => ({
+  Script: () => (
       <Script>
         {`
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${props.tagId ?? TAG_ID}');
+})(window,document,'script','dataLayer','${tagId ?? TAG_ID}');
         `}
       </Script>
+  ),
+  NoScript: () => (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${tagId}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
   )
-}
+});
+
--- packages/web-application/app/layout.tsx
+++ packages/web-application/app/layout.tsx
@@ -1,22 +1,18 @@
-import { GoogleAnalytics } from "theme";
+import { GoogleAnalytics, GoogleTagManager } from "theme";
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const Tag = GoogleTagManager();
   return (
     <html lang="ja">
       <GoogleAnalytics />
+      <Tag.Script />
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MS7Q6JFP"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
+        <Tag.NoScript />
         {children}
       </body>
     </html>
